590faaf36660c2b79dd2998943b8f0102def9f32
Rog <alexanderogov@gmail.ru>
2017-02-26 20:55:42 +0300

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("dfg wwrtewr sadgfadsfg");
+        System.out.println("fgdfg wwrteqwewr sadgfadsfg");
+        System.out.println("fgdfg wwrtewr sadgfadwwesfg");
+        System.out.println("fgdfg wwrtsdfewr sadgfadwwesfg");
+    }
+}
