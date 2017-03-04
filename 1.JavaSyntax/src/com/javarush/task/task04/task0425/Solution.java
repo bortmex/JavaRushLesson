b2e2f815cf2aaf86fc4470658702e12f598c62d8
Rog <alexanderogov@gmail.ru>
2017-03-04 23:50:09 +0300

    lesson 4-7

--- 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -4,10 +4,19 @@ package com.javarush.task.task04.task0425;
 Цель установлена!
 */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        coordinatePlane();
+    }
+
+    public static void coordinatePlane() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int first = Integer.parseInt(reader.readLine());
+        int second = Integer.parseInt(reader.readLine());
+        System.out.println(first>0&&second>0 ? 1 : first> 0 && second<0 ? 4 : first<0 && second<0 ? 3 : 2);
     }
 }
