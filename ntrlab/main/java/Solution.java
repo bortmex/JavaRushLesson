e9ac7289ce1be914b878ad060109f6323f6ff4de
Rog <alexanderogov@gmail.com>
2017-10-20 16:50:53 +0300

    correct task for ntrlab

--- ntrlab/main/java/Solution.java
+++ ntrlab/main/java/Solution.java
@@ -1,22 +1,42 @@
-
 public class Solution {
     //test cubePlusOne
     public static void main(String[] args) {
-        int x = 0;
-        for (int i = 0; i < 10000000; i++) {
-            int y = cubePlusOne();
-            if(y==7) x++;
+        int[] arrx = new int[8];
+        for (int i = 0; i < 1000000; i++) {
+           arrx[cubePlusOne()]++;
         }
-        System.out.format("%(.7f",1.0/7);
+        System.out.print("Нужная вероятность: ");
+        System.out.format("%(.2f%%",(1.0/7)*100);
         System.out.println();
-        System.out.println(x*1.0/10000000);
+        System.out.println("--------------------------");
+        System.out.println("Мой результат: ");
+        for (int i = 0; i < 7; i++) {
+            System.out.format("%(.2f%%",arrx[i+1]*1.0/1000000*100);
+            System.out.println();
+        }
     }
 
     public static int cube(){
         return (int) (Math.random()*6) + 1;
-    }
+    }  //1-6
 
     public static int cubePlusOne(){
-        return (cube() + cube())%7 + 1;
+        int[][] arr = {
+                { 1, 2, 3, 4, 5, 6 },
+                { 7, 1, 2, 3, 4, 5 },
+                { 6, 7, 1, 2, 3, 4 },
+                { 5, 6, 7, 1, 2, 3 },
+                { 4, 5, 6, 7, 1, 2 },
+                { 3, 4, 5, 6, 7, 0 }
+        };
+
+        int result = 0;
+        while (result == 0){
+            int i = cube();
+            int j = cube();
+            result = arr[i-1][j-1];
+        }
+        return result;
     }
+
 }
